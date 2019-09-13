ecdc1f578553688d39bdc262adf7d083a1798508
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2019-09-14 05:36:08 +0800

    step8-pushgit
    https://medium.com/@hdsingh13/deploying-django-app-on-heroku-with-postgres-as-backend-b2f3194e8a43
    
    step 8 finished

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rais helmy\documents\github\heroku-django-postgresql\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
